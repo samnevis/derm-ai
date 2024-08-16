7121379fcfe4fbddc97d232a25202807b86fe5b7
samnevis2005 <samnevis2005@gmail.com>
2024-08-15 18:02:50 -0700

    fixes model loading

---
+++ skindoctor/aiskinsrc.py
@@ -0,0 +1,32 @@
+
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+
+
+def predict_image_class(image_path):
+    """
+    Predict the class probabilities of the input image using the model.
+
+    :param image_path: Path to the image file
+    :return: Predicted class probabilities
+    """
+    #os.chdir('skindoctor')
+    print(os.listdir())
+    print(os.path.exists('gender_model.keras'))
+
+    model = tf.keras.models.load_model("gender_model.keras")
+
+    img = cv2.imread(image_path)
+    resize = tf.image.resize(img, (256,256))
+
+    yhat = model.predict(np.expand_dims(resize/255, 0))
+
+    return yhat
+
+
+if __name__ == "__main__":
+    # print(keras.version())
+    test = predict_image_class('acne_test.jpeg')
+    print(test)
